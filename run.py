277be6f7b5638194320ab7cdf27720e9cebbdbe8
Finnbarr Ambrose <finnbarr.ambrose121@gmail.com>
2024-10-22 19:16:13 +0000

    retrieve last data entered

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -52,4 +53,33 @@ def validate_data(values):
     return True
 
 
-data = get_sales_data()
+def update_sales_worksheet(data):
+    """
+    update data add new row
+    """
+    print("updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales worksheet updated sussessfully.\n")
+
+def calculate_surplus_data(sales_row):
+    """
+    surpluse sums
+    """
+    print("calculating suplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    run all programs 
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("love sandwichs data auto")
+main()
